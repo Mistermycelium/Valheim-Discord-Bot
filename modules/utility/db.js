eced804355cdf10b8dacc94c37a5eee89ba57470
Cody Smith <macmanchomp@gmail.com>
2023-12-05 13:02:41 -0800

    refactor to use sequelize and local sqlite3.

--- modules/utility/db.js
+++ modules/utility/db.js
@@ -1,40 +1,64 @@
-const { mysqlinfo } = require('../../config.json');
-const mysql = require('mysql');
-const db = mysql.createConnection(mysqlinfo);
+const { Sequelize } = require('sequelize');
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Connected!');
+const db = new Sequelize('vhbot', 'VHBot', 'Taco', {
+  host: 'localhost',
+  dialect: 'sqlite',
+  logging: false,
+  // SQLite only
+  storage: './database.sqlite',
 });
 
+const Whitelist = db.define('whitelist', {
+  DiscordID: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  Username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  SteamID: {
+    type: Sequelize.STRING,
+    defaultValue: null,
+  },
+  XboxID: {
+    type: Sequelize.STRING,
+    defaultValue: null,
+  },
+});
+
+db.sync()
+  .then(() => {
+    console.log('Database & tables created!');
+  })
+  .catch((error) => {
+    console.error('This error occured', error);
+  });
+
 module.exports = {
-  getWhitelistData:
-    async function() {
-      const result = await new Promise((resolve, reject) => {
-        db.query('SELECT * FROM whitelist;', function(error, results, fields) {
-          if (error) reject(error);
-          resolve(results.map(row => JSON.parse(JSON.stringify(row))));
-        });
+  getWhitelistData: async function() {
+    await db.sync()
+      .then(() => {
+        console.log('Database & tables created!');
+      })
+      .catch((error) => {
+        console.error('This error occured', error);
       });
-      return result;
-    },
-  //   findUser: async function(id) {
-  //     db.query(`SELECT * FROM whitelist WHERE DiscordID = ${id};`, function(error, results, fields) {
-  //       if (error) throw error;
-  //       return results;
-  //     });
-  //   },
-  addUser: async function(usr) {
-    let sql = '';
-    if (usr.SteamID) {
-      sql = `INSERT INTO whitelist (DiscordID, Username, SteamID) VALUES ('${usr.DiscordID}', '${usr.Username}', '${usr.SteamID}')`;
-    } else if (usr.XboxID) {
-      sql = `INSERT INTO whitelist (DiscordID, Username, XboxID) VALUES ('${usr.DiscordID}', '${usr.Username}', '${usr.XboxID}')`;
-    }
-    db.query(sql, function(error, results, fields) {
-      if (error) throw error;
-      return results;
-    });
+    const result = await Whitelist.findAll();
+    return result;
+  },
+
+  addUser: async function(user) {
+    const result = await Whitelist.create(user);
+    await db.sync()
+      .then(() => {
+        console.log('Database & tables created!');
+      })
+      .catch((error) => {
+        console.error('This error occured', error);
+      });
+    return result;
   },
 
 };
---
+++ start.ps1
@@ -0,0 +1,10 @@
+$dir = "./"
+
+# Change the current directory to the specified directory
+Set-Location $dir
+
+# Install npm packages
+npm install
+
+# Run node index.js
+node index.js
